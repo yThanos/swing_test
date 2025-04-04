4bd0a343a26bcae5b321c88ed12079fee4c7ff83
vitor.rosmann <vitor.rosmann@avmb.com.br>
2025-04-04 16:33:37 -0300

    fala dele, kkkkkk vsfd

--- src/main/java/dev/fraporti/clicker/MainFrame.java
+++ src/main/java/dev/fraporti/clicker/MainFrame.java
@@ -1,6 +1,7 @@
 package dev.fraporti.clicker;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -10,6 +11,8 @@ import java.util.concurrent.TimeUnit;
  * @author vitor.rosmann on 19/03/2025
  */
 public class MainFrame extends JFrame {
+    private static MainFrame mainFrame;
+
     //cookies
     private final static JButton cookie = new JButton();
     private static final JLabel cookies = new JLabel();
@@ -25,6 +28,10 @@ public class MainFrame extends JFrame {
     private static int clickerCPS = 0;
     private static int clickerPrice = 100;
 
+    //grandma
+    private static final JLabel grandmaLabel = new JLabel();
+    private static int grandmaCPS = 0;
+    private static int grandmaPrice = 1000;
 
     //scheduler for passive income
     private static final ScheduledExecutorService scheduler = new ScheduledThreadPoolExecutor(1);
@@ -32,10 +39,12 @@ public class MainFrame extends JFrame {
     //synchronized monitor
     private static final Object monitor = new Object();
 
-    public MainFrame() throws IOException {
+    public MainFrame() {
         this.setTitle("Cookie clicker DEMO");
         this.setSize(800, 200);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setLayout(new GridLayout(1,2));
 
         //cookie button
         JPanel cookiesPanel = getCookiesPanel();
@@ -44,41 +53,66 @@ public class MainFrame extends JFrame {
         //Click
         JPanel upgradesPanel = getUpgradesPanel();
         this.add(upgradesPanel);
-
-
     }
 
     private static JPanel getCookiesPanel() {
         JPanel cookiesPanel = new JPanel();
-        cookiesPanel.setBounds(0,0,200,100);
-        cookie.setText("COOKIE");
-        cookie.addActionListener(e-> incrementCookieAmount(TypeOfCookieIncrement.CLICK));
-        cookiesPanel.add(cookie);
+        cookiesPanel.setLayout(new BoxLayout(cookiesPanel, BoxLayout.Y_AXIS));
+
+        cookiesPanel.add(Box.createVerticalStrut(40));
 
         //cookie amount label
         cookies.setText(cookieAmount + " cookies");
+        cookies.setAlignmentX(Component.CENTER_ALIGNMENT);
         cookiesPanel.add(cookies);
+
+        cookie.setText("COOKIE");
+        cookie.addActionListener(e-> incrementCookieAmount(TypeOfCookieIncrement.CLICK));
+        cookie.setAlignmentX(Component.CENTER_ALIGNMENT);
+        cookiesPanel.add(cookie);
+
         return cookiesPanel;
     }
 
     private static JPanel getUpgradesPanel() {
         //panel
         JPanel upgradesPanel = new JPanel();
-        upgradesPanel.setBounds(0, 100, 400, 100);
+        upgradesPanel.setLayout(new BoxLayout(upgradesPanel, BoxLayout.Y_AXIS));
+
+        clickIncrementLabel.setText("$" + clickIncrementPrice + " cookies");
+        clickIncrementLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        upgradesPanel.add(clickIncrementLabel);
+
         //increment click button
         JButton incrementClickMultiplier = new JButton("Increment click strength");
         incrementClickMultiplier.addActionListener(MainFrame::incrementClickStrength);
+        incrementClickMultiplier.setAlignmentX(Component.CENTER_ALIGNMENT);
         upgradesPanel.add(incrementClickMultiplier);
-        clickIncrementLabel.setText("$" + clickIncrementPrice + " cookies");
-        upgradesPanel.add(clickIncrementLabel);
+
+        upgradesPanel.add(Box.createVerticalStrut(10));
+
+        clickerLabel.setText("$" + clickerPrice + " cookies");
+        clickerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        upgradesPanel.add(clickerLabel);
 
         //buy clicker
         JButton buyClicker = new JButton("Buy 1 clicker");
         buyClicker.addActionListener(MainFrame::buyClicker);
-        clickerLabel.setText("$" + clickerPrice + " cookies");
-
+        buyClicker.setAlignmentX(Component.CENTER_ALIGNMENT);
         upgradesPanel.add(buyClicker);
-        upgradesPanel.add(clickerLabel);
+
+        upgradesPanel.add(Box.createVerticalStrut(10));
+
+        grandmaLabel.setText("$" + grandmaPrice + " cookies");
+        grandmaLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        upgradesPanel.add(grandmaLabel);
+
+        //buy grandma
+        JButton buyGrandma = new JButton("Buy 1 grandma");
+        buyGrandma.addActionListener(MainFrame::buyGrandma);
+        buyGrandma.setAlignmentX(Component.CENTER_ALIGNMENT);
+        upgradesPanel.add(buyGrandma);
+
         return upgradesPanel;
     }
 
@@ -86,7 +120,6 @@ public class MainFrame extends JFrame {
         synchronized (monitor){
             if(cookieAmount >= clickerPrice){
                 cookieAmount -= clickerPrice;
-                clickerLabel.setText("$" + clickerPrice + " cookies");
                 clickerPrice = (int) (clickerPrice * 1.5);
                 if(clickerCPS < 2){
                     clickerCPS = 2;
@@ -95,6 +128,9 @@ public class MainFrame extends JFrame {
                     clickerCPS = (int)(clickerCPS * 1.5);
                 }
                 cookies.setText(cookieAmount + " cookies");
+                clickerLabel.setText("$" + clickerPrice + " cookies");
+            } else {
+                JOptionPane.showMessageDialog(mainFrame, "Not enough cookies!");
             }
         }
     }
@@ -107,6 +143,8 @@ public class MainFrame extends JFrame {
                 clickIncrementPrice = (int) (clickIncrementPrice * 1.2);
                 clickIncrementLabel.setText("$" + clickIncrementPrice + " cookies");
                 cookies.setText(cookieAmount + " cookies");
+            } else {
+                JOptionPane.showMessageDialog(mainFrame, "Not enough cookies!");
             }
         }
     }
@@ -125,12 +163,32 @@ public class MainFrame extends JFrame {
         }
     }
 
-    private static enum TypeOfCookieIncrement {
+    private static void buyGrandma(Object e){
+        synchronized (monitor){
+            if(cookieAmount >= grandmaPrice){
+                cookieAmount -= grandmaPrice;
+                grandmaLabel.setText("$" + grandmaPrice + " cookies");
+                grandmaPrice = (int) (grandmaPrice * 1.5);
+                if(grandmaCPS < 2){
+                    grandmaCPS = 10;
+                    scheduler.scheduleAtFixedRate(()->incrementCookieAmount(TypeOfCookieIncrement.CLICKER), 0, 1, TimeUnit.SECONDS);
+                } else {
+                    grandmaCPS = (int)(grandmaCPS * 1.5);
+                }
+                cookies.setText(cookieAmount + " cookies");
+            } else {
+                JOptionPane.showMessageDialog(mainFrame, "Not enough cookies!");
+            }
+        }
+    }
+
+    private enum TypeOfCookieIncrement {
         CLICK,
         CLICKER
     }
 
     public static void main(String[] args) throws IOException {
-        new MainFrame().setVisible(true);
+        mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
     }
 }
